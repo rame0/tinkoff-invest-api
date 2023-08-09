bf267179a5866469d73042a5593d24e4bc414f4f
rame0 <r@me0.biz>
2023-08-09 08:03:18 +0600

    fix: check if `unzip` or `7z` is installed

--- scripts/pull-proto.sh
+++ scripts/pull-proto.sh
@@ -5,5 +5,14 @@
 set -euo pipefail
 
 wget "https://github.com/Tinkoff/investAPI/archive/master.zip" -O proto.zip
-unzip -o proto.zip
+
+if command -v unzip &>/dev/null; then
+  unzip -o proto.zip
+elif command -v 7z &>/dev/null; then
+  7z x proto.zip
+else
+  echo "unzip or 7z required"
+  exit 1
+fi
+
 rm -f proto.zip
